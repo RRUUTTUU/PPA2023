c1accf168619294f36fc78544c512aebc33b3303
RRUUTTUU <148451541+RRUUTTUU@users.noreply.github.com>
2023-11-29 20:26:40 +0530

    Add files via upload

---
+++ Commandline.java
@@ -0,0 +1,14 @@
+class Commandline
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Number of Command line arguments are :"+arg.length());
+
+        System.out.println("Command line argument are: ");
+
+        for(int i = 0; i < arg.leng; i+                    +)
+        {
+            System.out.println(arg[i]);
+        }
+    }
+}
